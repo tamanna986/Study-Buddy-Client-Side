fbb7d92a2e144d322b308c72653cbc3c866e1446
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-05 22:22:50 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLRZcpXoiQEv9_ZIEHeu7pES1zl8q15kM",
+  authDomain: "study-buddy-ce97a.firebaseapp.com",
+  projectId: "study-buddy-ce97a",
+  storageBucket: "study-buddy-ce97a.appspot.com",
+  messagingSenderId: "30758312313",
+  appId: "1:30758312313:web:ce0da39eef3e1c245f77a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
