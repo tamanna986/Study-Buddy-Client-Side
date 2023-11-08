05e13562b33dd2498b04c46b0ea508c02a22ce81
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-08 09:51:13 +0600

    assignments created and sent them to database

--- src/pages/CreateAssignments/CreateAssignments.jsx
+++ src/pages/CreateAssignments/CreateAssignments.jsx
@@ -1,27 +1,66 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+import DatePick from "./DatePick";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import { useRef } from "react";
 
 
 const CreateAssignments = () => {
+    const {user} = useContext(AuthContext)
+    const formRef = useRef(null);
+    const email = user.email
+    
+// State for the selected due date
+    const [dueDate, setDueDate] = useState(new Date()); 
+
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
         const form = e.target;
         const photo = form.photo.value;
-        const name = form.name.value;
-        const brandName = form.brandName.value;
-        const category = form.category.value;
-        const price = form.price.value;
+        const title = form.title.value;
         const description = form.description.value;
-        const rating = form.rating.value;
+        const marks = form.marks.value;
+        const category = form.category.value;
+        
+        
 
-        const newProduct = { photo, name, brandName, category, price, description, rating };
-        console.log(newProduct)
+        const newAssignment = { photo, title, description,marks, category, dueDate: dueDate , email};
+        console.log(newAssignment)
+
+
+        fetch('http://localhost:5000/assignments' , {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newAssignment)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Assignment created Successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Close'
+                  })
+            }
+            formRef.current.reset(); // Reset the form after successful form submission
+        })
+    
+
+
+
     }
     return (
         <div className="my-10">
             <h1 className="text-3xl font-bold text-sky-900 text-center mt-10">Create Assignment</h1>
-            <form onSubmit={handleFormSubmit}>
+            <form ref={formRef} onSubmit={handleFormSubmit}>
                 <div className=" w-[300px] md:w-[500px] lg:w-[600px] mx-auto">
-
+                      
                     <div className="card-body ">
                         <div className="flex justify-center items-center">
 
@@ -45,7 +84,7 @@ const CreateAssignments = () => {
                             <input
                                 type="text"
                                 placeholder="Assignment Title"
-                                name="name"
+                                name="title"
 
                                 required
                                 className="input input-bordered input-warning w-full" />
@@ -59,7 +98,7 @@ const CreateAssignments = () => {
                             <input
                                 type="text"
                                 placeholder="Description"
-                                name="Description"
+                                name="description"
 
                                 required
                                 className="input input-bordered input-warning w-full" />
@@ -70,9 +109,9 @@ const CreateAssignments = () => {
                             </label>
 
                             <input
-                                type="text"
+                                type="number"
                                 placeholder="Marks"
-                                name="Marks"
+                                name="marks"
 
                                 required
                                 className="input input-bordered input-warning w-full" />
@@ -91,6 +130,15 @@ const CreateAssignments = () => {
 
                             </select>
                         </div>
+                        <div className="flex  items-center gap-3">
+                    <label className="label">
+                        <span className="label-text t text-lg py-3 px-12 bg-yellow-100  text-sky-700 font-semibold rounded-lg">
+                            Due Date
+                        </span>
+                    </label>
+                    
+                    <DatePick selected={dueDate} setStartDate={setDueDate} />
+                </div>
 
                         
                         
